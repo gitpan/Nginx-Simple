364695bec9bd099b151571e365ffab64105a2e3f
Michael J. Flickinger <mjflick@gnu.org>
2008-12-09 01:58:32 +0000

    Import of MJFLICK/Nginx-Simple-0.04 from CPAN.
    gitpan-cpan-distribution: Nginx-Simple
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MJFLICK/Nginx-Simple-0.04.tar.gz
    gitpan-cpan-author:       MJFLICK
    gitpan-cpan-maturity:     released

--- lib/Nginx/Simple.pm
+++ lib/Nginx/Simple.pm
@@ -1,7 +1,7 @@
 package Nginx::Simple;
 use Exporter;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 @ISA = qw(Exporter);
 
@@ -245,7 +245,7 @@ sub status
     }
     else
     {
-        return $status;
+        return $request_data{status};
     }
 }
 
